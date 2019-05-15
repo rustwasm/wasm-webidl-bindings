a9eadb4a36117d32f6b3b0f659329ea178a55c24
Nick Fitzgerald <fitzgen@gmail.com>
2019-05-15 13:23:09 -0700

    Add a little more details to the README

--- src/lib.rs
+++ src/lib.rs
@@ -7,9 +7,28 @@
 [![](https://img.shields.io/crates/d/wasm-webidl-bindings.svg)](https://crates.io/crates/wasm-webidl-bindings)
 [![Build Status](https://dev.azure.com/rustwasm/wasm-webidl-bindings/_apis/build/status/rustwasm.wasm-webidl-bindings?branchName=master)](https://dev.azure.com/rustwasm/wasm-webidl-bindings/_build/latest?definitionId=2&branchName=master)
 
-# Example
+## What's Inside
+
+* A parser for the straw proposal text format. See `src/grammar.lalrpop`.
+
+* A set of AST types for representing and manipulating WebIDL bindings. See
+  `src/ast.rs`.
+
+## Example
 
 TODO
+
+## TODO
+
+* [ ] Write an AST out as straw proposal text format.
+
+* [ ] Validate and canonicalize ASTs.
+
+* [ ] Read a straw proposal binary encoding.
+
+* [ ] Write a straw proposal binary encoding.
+
+* [ ] Support optional `type=` and `idx=` named parameters in the text format.
 
  */
 #![deny(missing_debug_implementations)]
