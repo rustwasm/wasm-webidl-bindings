591a5f35991520cd28359987de97659bafe8bef9
Nick Fitzgerald <fitzgen@gmail.com>
2019-05-20 15:27:59 -0700

    Add support for encoding the type subsection

--- BINARY.md
+++ BINARY.md
@@ -2,3 +2,144 @@
 
 This document describes the straw proposal binary format that this crate
 currently supports.
+
+<!-- START doctoc generated TOC please keep comment here to allow auto update -->
+<!-- DON'T EDIT THIS SECTION, INSTEAD RE-RUN doctoc TO UPDATE -->
+
+
+- [The Web IDL Bindings Custom Section](#the-web-idl-bindings-custom-section)
+- [Subsections](#subsections)
+- [The Web IDL Type Subsection](#the-web-idl-type-subsection)
+  - [Web IDL Functions](#web-idl-functions)
+  - [Web IDL Dictionaries](#web-idl-dictionaries)
+  - [Web IDL Enumerations](#web-idl-enumerations)
+  - [Web IDL Unions](#web-idl-unions)
+- [References to Web IDL Types](#references-to-web-idl-types)
+- [The Function Binding Subsection](#the-function-binding-subsection)
+
+<!-- END doctoc generated TOC please keep comment here to allow auto update -->
+
+## The Web IDL Bindings Custom Section
+
+**Custom section name:** `webidl-bindings`.
+
+## Subsections
+
+The data of a Web IDL Bindings custom section contains a sequence of
+subsections. Each subsection consists of:
+
+* a one-byte *id*,
+* a `u32` *size* of its contents, in bytes,
+* the subsection's actual *contents*, whose structure is determined by its
+  subsection *id*.
+
+```
+webidl_bindings_sec ::= section[0](webidl_bindings_data)
+
+webidl_bindings_data ::= n:name                 (if name = "webidl-bindings")
+                         webidl_type_subsec?
+                         bindings_subsec
+
+webidl_bindings_subsection[N](B) ::= N:byte size:u32 B   (if size = |B|)
+```
+
+The following subsection ids are used:
+
+| Id | Subsection              |
+|:--:|:------------------------|
+| 0  | Web IDL Type Subsection |
+| 1  | Bindings Subsection     |
+
+## The Web IDL Type Subsection
+
+The Web IDL Type Subsection is a sequence of `webidl_type` definitions:
+
+```
+webidl_type_subsec ::= webidl_bindings_subsection[0](vec(webidl_type))
+```
+
+A `webidl_type` is a function, dictionary, enumeration, or union:
+
+```
+webidl_type ::= 0x0 webidl_function
+            ::= 0x1 webidl_dictionary
+            ::= 0x2 webidl_enumeration
+            ::= 0x3 webidl_union
+```
+
+### Web IDL Functions
+
+Functions are encoded as their function kind, which is one of
+
+* static (0x0)
+* method and receiver type (0x1)
+* constructor (0x2)
+
+followed by a sequence of references to their parameter types, and an optional
+reference to its result type:
+
+```
+webidl_function ::= webidl_function_kind
+                    vec(webidl_function_param)
+                    webidl_function_result
+
+webidl_function_kind ::= 0x0                       # static
+                     ::= 0x1 webidl_type_reference # method
+                     ::= 0x2                       # constructor
+
+webidl_function_param ::= webidl_type_reference
+
+webidl_function_result ::= 0x0
+                       ::= 0x1 webidl_type_reference
+```
+
+### Web IDL Dictionaries
+
+Dictionaries are encodes as a `vec` of pairs of the dictionary field's UTF-8
+name string and a reference to the field's value:
+
+```
+webidl_dictionary ::= vec(webidl_dictionary_field)
+
+webidl_dictionary_field ::= name webidl_type_reference
+```
+
+### Web IDL Enumerations
+
+Enumerations are encoded as a `vec` of their values' UTF-8 name strings.
+
+```
+webidl_enumeration ::= vec(name)
+```
+
+### Web IDL Unions
+
+Unions are encoded as a `vec` of references to their member types:
+
+```
+webidl_union ::= vec(webidl_type_reference)
+```
+
+## References to Web IDL Types
+
+References to Web IDL types appear in both the Web IDL Type Subsection and in
+the Web IDL Function Binding Subsection.
+
+References to Web IDL types are encoded as `i32`s and come in two forms:
+
+1. References to compound Web IDL types are `>= 0` and are indices referencing
+   the `i`th compound type defined in the Web IDL Type Subsection.
+2. Primitive Web IDL values are encoded as negative numbers, and each primitive
+   type is special cased.
+
+```
+webidl_type_reference ::= i:i32    (if i >= 0)     => index into Web IDL Type Subsection
+                      ::= i:i32    (if i == -1)    => TODO(web idl primitive types...)
+                      ::= i:i32    (if i == -2)    => TODO(web idl primitive types...)
+                      ::= i:i32    (if i == -3)    => TODO(web idl primitive types...)
+                      # etc...
+```
+
+## The Function Binding Subsection
+
+TODO
--- src/binary/encode.rs
+++ src/binary/encode.rs
@@ -1,12 +1,401 @@
 use crate::ast;
 use std::io;
 
-pub(crate) trait Encode<W: io::Write> {
+pub(crate) trait Encode<W: ?Sized + io::Write> {
     fn encode(&self, w: &mut W) -> io::Result<()>;
 }
 
-impl<W: io::Write> Encode<W> for ast::WebidlBindingsSection {
+trait WriteExt: io::Write {
+    fn byte(&mut self, b: u8) -> io::Result<()> {
+        self.write_all(&[b])?;
+        Ok(())
+    }
+
+    fn uleb(&mut self, val: u32) -> io::Result<()> {
+        leb128::write::unsigned(self, val as u64)?;
+        Ok(())
+    }
+
+    fn ileb(&mut self, val: i32) -> io::Result<()> {
+        leb128::write::signed(self, val as i64)?;
+        Ok(())
+    }
+
+    fn vec<E>(&mut self, items: &[E]) -> io::Result<()>
+    where
+        E: Encode<Self>,
+    {
+        self.uleb(items.len() as u32)?;
+        for x in items {
+            x.encode(self)?;
+        }
+        Ok(())
+    }
+}
+
+impl<W> WriteExt for W where W: ?Sized + io::Write {}
+
+impl<W: ?Sized + io::Write> Encode<W> for String {
     fn encode(&self, w: &mut W) -> io::Result<()> {
+        w.uleb(self.len() as u32)?;
+        w.write_all(self.as_bytes())
+    }
+}
+
+impl<W: ?Sized + io::Write> Encode<W> for ast::WebidlBindingsSection {
+    fn encode(&self, w: &mut W) -> io::Result<()> {
+        self.types.encode(w)?;
         unimplemented!()
+    }
+}
+
+impl<W: ?Sized + io::Write> Encode<W> for ast::WebidlTypeSubsection {
+    fn encode(&self, w: &mut W) -> io::Result<()> {
+        w.byte(0)?;
+        w.vec(&self.types)
+    }
+}
+
+impl<W: ?Sized + io::Write> Encode<W> for ast::WebidlType {
+    fn encode(&self, w: &mut W) -> io::Result<()> {
+        self.ty.encode(w)
+    }
+}
+
+impl<W: ?Sized + io::Write> Encode<W> for ast::WebidlCompoundType {
+    fn encode(&self, w: &mut W) -> io::Result<()> {
+        match self {
+            ast::WebidlCompoundType::Function(f) => {
+                w.byte(0)?;
+                f.encode(w)
+            }
+            ast::WebidlCompoundType::Dictionary(d) => {
+                w.byte(1)?;
+                d.encode(w)
+            }
+            ast::WebidlCompoundType::Enumeration(e) => {
+                w.byte(2)?;
+                e.encode(w)
+            }
+            ast::WebidlCompoundType::Union(u) => {
+                w.byte(3)?;
+                u.encode(w)
+            }
+        }
+    }
+}
+
+impl<W: ?Sized + io::Write> Encode<W> for ast::WebidlFunction {
+    fn encode(&self, w: &mut W) -> io::Result<()> {
+        self.kind.encode(w)?;
+        w.vec(&self.params)?;
+        if let Some(result) = self.result.as_ref() {
+            w.byte(1)?;
+            result.encode(w)
+        } else {
+            w.byte(0)
+        }
+    }
+}
+
+impl<W: ?Sized + io::Write> Encode<W> for ast::WebidlFunctionKind {
+    fn encode(&self, w: &mut W) -> io::Result<()> {
+        match self {
+            ast::WebidlFunctionKind::Static => w.byte(0),
+            ast::WebidlFunctionKind::Method(m) => {
+                w.byte(1)?;
+                m.ty.encode(w)
+            }
+            ast::WebidlFunctionKind::Constructor => w.byte(2),
+        }
+    }
+}
+
+impl<W: ?Sized + io::Write> Encode<W> for ast::WebidlTypeRef {
+    fn encode(&self, w: &mut W) -> io::Result<()> {
+        match self {
+            ast::WebidlTypeRef::Indexed(i) => w.ileb(i.idx as i32),
+            // TODO: primitive types:
+            // https://github.com/rustwasm/wasm-webidl-bindings/issues/8
+            ast::WebidlTypeRef::Named(_) => panic!("can only encode canonicalized ASTs"),
+        }
+    }
+}
+
+impl<W: ?Sized + io::Write> Encode<W> for ast::WebidlDictionary {
+    fn encode(&self, w: &mut W) -> io::Result<()> {
+        w.vec(&self.fields)
+    }
+}
+
+impl<W: ?Sized + io::Write> Encode<W> for ast::WebidlDictionaryField {
+    fn encode(&self, w: &mut W) -> io::Result<()> {
+        self.name.encode(w)?;
+        self.ty.encode(w)
+    }
+}
+
+impl<W: ?Sized + io::Write> Encode<W> for ast::WebidlEnumeration {
+    fn encode(&self, w: &mut W) -> io::Result<()> {
+        w.vec(&self.values)
+    }
+}
+
+impl<W: ?Sized + io::Write> Encode<W> for ast::WebidlUnion {
+    fn encode(&self, w: &mut W) -> io::Result<()> {
+        w.vec(&self.members)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::ast::*;
+
+    macro_rules! assert_encoding {
+        (
+            $(
+                $name:ident($ast:expr, $expected:expr $(,)* );
+            )*
+        ) => {
+            $(
+                #[test]
+                fn $name() {
+                    let mut buf = vec![];
+                    $ast.encode(&mut buf).expect("writing into a vec can't fail");
+                    assert_eq!(buf, &$expected);
+                }
+            )*
+        }
+    }
+
+    // SLEB128 = [185, 10]
+    const WEBIDL_TYPE_REF_A: WebidlTypeRef =
+        WebidlTypeRef::Indexed(WebidlTypeRefIndexed { idx: 1337 });
+    // SLEB128 = [169, 198, 0]
+    const WEBIDL_TYPE_REF_B: WebidlTypeRef =
+        WebidlTypeRef::Indexed(WebidlTypeRefIndexed { idx: 9001 });
+
+    assert_encoding! {
+        webidl_type_function(
+            WebidlType {
+                name: None,
+                ty: WebidlCompoundType::Function(WebidlFunction {
+                    kind: WebidlFunctionKind::Static,
+                    params: vec![],
+                    result: None
+                }),
+            },
+            [
+                // Function kind
+                0,
+                // Static kind
+                0,
+                // Number of params
+                0,
+                // Has result?
+                0,
+            ]
+        );
+        webidl_type_dictionary(
+            WebidlType {
+                name: None,
+                ty: WebidlCompoundType::Dictionary(WebidlDictionary {
+                    fields: vec![
+                        WebidlDictionaryField { name: "first".into(), ty: WEBIDL_TYPE_REF_A, },
+                        WebidlDictionaryField { name: "second".into(), ty: WEBIDL_TYPE_REF_B, },
+                    ],
+                }),
+            },
+            [
+                // Dictionary type
+                1,
+                // Number of fields
+                2,
+                // "first"
+                5, 102, 105, 114, 115, 116,
+                // WEBIDL_TYPE_REF_A
+                185, 10,
+                // "second"
+                6, 115, 101, 99, 111, 110, 100,
+                // WEBIDL_TYPE_REF_B
+                169, 198, 0,
+            ],
+        );
+        webidl_type_enumeration(
+            WebidlType {
+                name: None,
+                ty: WebidlCompoundType::Enumeration(WebidlEnumeration {
+                    values: vec!["hi".into(), "bye".into()],
+                }),
+            },
+            [
+                // Enumeration type
+                2,
+                // Number of values
+                2,
+                // "hi"
+                2, 104, 105,
+                // "bye"
+                3, 98, 121, 101,
+            ]
+        );
+        webidl_type_union(
+            WebidlType {
+                name: None,
+                ty: WebidlCompoundType::Union(WebidlUnion {
+                    members: vec![WEBIDL_TYPE_REF_A, WEBIDL_TYPE_REF_B]
+                }),
+            },
+            [
+                // Union type
+                3,
+                // Number of members
+                2,
+                // WEBIDL_TYPE_REF_A
+                185, 10,
+                // WEBIDL_TYPE_REF_B
+                169, 198, 0,
+            ]
+        );
+
+        webidl_function_static(
+            WebidlFunction {
+                kind: WebidlFunctionKind::Static,
+                params: vec![],
+                result: None
+            },
+            [
+                // Static kind
+                0,
+                // Number of params
+                0,
+                // Has result?
+                0,
+            ]
+        );
+        webidl_function_method(
+            WebidlFunction {
+                kind: WebidlFunctionKind::Method(WebidlFunctionKindMethod {
+                    ty: WEBIDL_TYPE_REF_A,
+                }),
+                params: vec![],
+                result: None
+            },
+            [
+                // Method kind
+                1,
+                // WEBIDL_TYPE_REF_A
+                185, 10,
+                // Number of params
+                0,
+                // Has result?
+                0,
+            ]
+        );
+        webidl_function_constructor(
+            WebidlFunction {
+                kind: WebidlFunctionKind::Constructor,
+                params: vec![],
+                result: None
+            },
+            [
+                // Constructor kind
+                2,
+                // Number of params
+                0,
+                // Has result?
+                0,
+            ]
+        );
+        webidl_function_params(
+            WebidlFunction {
+                kind: WebidlFunctionKind::Static,
+                params: vec![WEBIDL_TYPE_REF_A, WEBIDL_TYPE_REF_B],
+                result: None
+            },
+            [
+                // Static kind
+                0,
+                // Number of params
+                2,
+                // WEBIDL_TYPE_REF_A
+                185, 10,
+                // WEBIDL_TYPE_REF_B
+                169, 198, 0,
+                // Has result?
+                0,
+            ]
+        );
+        webidl_function_result(
+            WebidlFunction {
+                kind: WebidlFunctionKind::Static,
+                params: vec![],
+                result: Some(WEBIDL_TYPE_REF_A),
+            },
+            [
+                // Static kind
+                0,
+                // Number of params
+                0,
+                // Has result?
+                1,
+                // WEBIDL_TYPE_REF_A
+                185, 10,
+            ]
+        );
+
+        webidl_dictionary(
+            WebidlDictionary {
+                fields: vec![
+                    WebidlDictionaryField { name: "first".into(), ty: WEBIDL_TYPE_REF_A, },
+                    WebidlDictionaryField { name: "second".into(), ty: WEBIDL_TYPE_REF_B, },
+                ],
+            },
+            [
+                // Number of fields
+                2,
+                // "first"
+                5, 102, 105, 114, 115, 116,
+                // WEBIDL_TYPE_REF_A
+                185, 10,
+                // "second"
+                6, 115, 101, 99, 111, 110, 100,
+                // WEBIDL_TYPE_REF_B
+                169, 198, 0,
+            ],
+        );
+
+        webidl_enumeration(
+            WebidlEnumeration {
+                values: vec!["a".into(), "b".into(), "c".into()],
+            },
+            [
+                // Number of values
+                3,
+                // "a"
+                1, 97,
+                // "b"
+                1, 98,
+                // "c"
+                1, 99,
+            ],
+        );
+
+        webidl_union(
+            WebidlUnion {
+                members: vec![WEBIDL_TYPE_REF_A, WEBIDL_TYPE_REF_B],
+            },
+            [
+                // Number of members
+                2,
+                // WEBIDL_TYPE_REF_A
+                185, 10,
+                // WEBIDL_TYPE_REF_B
+                169, 198, 0,
+            ],
+        );
+
+        webidl_type_ref(WEBIDL_TYPE_REF_A, [185, 10]);
     }
 }
